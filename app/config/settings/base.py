94a0d64dc11e1f66c04c1503ee8acfe1b3d43064
LeeHanYeong <dev@lhy.kr>
2019-11-23 22:21:44 +0900

    DefaultPagination지정

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -110,6 +110,11 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    # Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 100,
+
+    # Custom
     'JSON_UNDERSCOREIZE': {
         'no_underscore_before_number': True,
     },
